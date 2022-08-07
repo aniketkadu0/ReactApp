9a389cb255921d49abeb8608c289aee0a2fc605b
aniketkadu0 <aniketkadu765@gmail.com>
2022-08-07 16:40:09 +0530

    hey yeh

--- src/Rooms.js
+++ src/Rooms.js
@@ -4,19 +4,19 @@ import "./index.css"
 export default function Rooms() {
   const navigate = useNavigate();
   const numbers1 = [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1 , 1 , 0, 1 , 1, 1];
-  const numbers2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1 , 1 , 0, 1 , 1, 1];
+  const numbers2 = [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1 , 1 , 0, 1 , 1, 1];
 
   const listItems1 = numbers1.map((numbers,index) =>
     <button
-    className={numbers ? "green" : "red"}
+    className={numbers ? "btn btn-success" : "btn btn-secondary" }
     disabled={!numbers}
     key={index}
     onClick= {() => {
       navigate('/new', {
         state : index+1
       });
-    }}
-    >Room no. {(index+1)}
+    }}>
+    Room no. {(index+1)}
     <br/>
     {numbers ? " is Available" : " is not Available"} 
     </button>
@@ -24,7 +24,7 @@ export default function Rooms() {
 
   const listItems2 = numbers2.map((numbers,index) =>
     <button
-    className={numbers ? "green" : "red"}
+    className={numbers ? "btn btn-success" : "btn btn-secondary"}
     disabled={!numbers}
     key={index}
     onClick= {() => {
@@ -40,12 +40,8 @@ export default function Rooms() {
 
   return (
     <>
-      <style>{`
-        .red {color: red; background-color: silver; margin : 15px; font-size:18px}
-        .green {color: green; background-color: white; margin : 15px; font-size:18px}
-      `}</style>
       {listItems1}
-      <hr style={{padding:"80px"}}></hr>
+      <hr style={{padding:"50px"}}></hr>
       <hr></hr>
       {listItems2}
     </>
